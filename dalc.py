85d7f49b50611a313d7c33be67f6d1827efe3117
Dalir Absent <dalirabsent@gmail.com>
2024-06-13 12:36:41 +0330

    The main Dalc program

---
+++ dalc.py
@@ -0,0 +1,26 @@
+import readline
+from _core.setting import running
+from _core.receiving import receive
+from _core.responding import response
+from _core.history import recording
+
+
+def main() -> None:
+    """
+    The main function to starts Dalc.
+    Gets the data from `_core.receiving`, saves it in the history
+    then passes it to `_core.responding` `response`, to get
+    a result for printing.
+    if the data is `None` prints nothing.
+    """
+    data = receive()
+
+    recording(data)
+    result = response(data)
+    if result is not None:
+        print(result)
+
+
+if __name__ == "__main__":
+    while running:
+        main()
