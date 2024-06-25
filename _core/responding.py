1e12c39c81b0b1d213e1fe8440446cc1fcea87ba
Dalir Absent <dalirabsent@gmail.com>
2024-06-25 13:49:44 +0330

    Adding a condition to the file

--- _core/responding.py
+++ _core/responding.py
@@ -6,15 +6,20 @@ from _core.frontend import shaping
 def response(data: str) -> str | None:
     """
     Passes the input data to `_core.processing` function.
-    If result is a function tries to run it and then returns `None`, otherwise
+    If the result is a function tries to run it and then returns `None`, otherwise
     prepares the result to print by `_core.frontend` `shaping`.
+    if the result is `None` returns `None`.
     """
     result = computing(data)
-
+    
+    if result is None:
+        return None
+    
     if type(result) is FunctionType:
         try:
             result()
             return None
+
         except Exception:
             answer = shaping(Exception)
             return answer
